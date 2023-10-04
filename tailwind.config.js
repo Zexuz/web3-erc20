f5379c2dade905a081353fd572231b8199a10a90
Zexuz <robin.edbom@gmail.com>
2023-10-04 18:12:58 +0100

    Add boilerplate theme and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "6rem",
+    },
+    colors: {
+      primary: {
+        500: "#2C50ED",
+      },
+      secondary: {
+        500: "#FF3366",
+      },
+      white: "#FFFFFF",
+      gray: {
+        100: "#F5F5F5",
+        200: "#E5E5E5",
+        300: "#D4D4D4",
+        400: "#A3A3A3",
+        500: "#656673",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+      },
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        bold: 700,
+        semibold: 600,
+        medium: 500,
+        regular: 400,
+        light: 300,
+      },
+    },
   },
   plugins: [],
-}
+};
