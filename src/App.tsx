a23503022eb9f1de074e74fd6805a383431036d8
Zexuz <robin.edbom@gmail.com>
2023-10-04 17:40:24 +0100

    Init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <p>Hello World!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
