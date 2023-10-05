9d47cd2b2f04a9539a66f22a0bb9e24fe8e4b556
Zexuz <robin.edbom@gmail.com>
2023-10-05 10:46:53 +0100

    Update input UI

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -36,9 +36,6 @@ export const Input = ({
     ? "bg-red-50 border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 dark:text-red-500 dark:placeholder-red-500 dark:border-red-500"
     : "";
 
-  const baseClasses =
-    "rounded-lg border p-2.5 text-gray-200 focus:border-primary focus:outline-none text-sm bg-blue-300 focus:ring-2 focus:ring-blue-400";
-
   return (
     <div className={"flex w-full flex-col space-y-2"}>
       <label className={`font-poppins text-sm font-medium ${errorClasses}`}>
@@ -46,7 +43,7 @@ export const Input = ({
       </label>
       <input
         type="text"
-        className={`${baseClasses} ${errorInputClasses} ${disabledClasses}`}
+        className={`rounded-lg border bg-blue-300 p-2.5 text-sm text-gray-200 hover:ring-2 hover:ring-blue-200 focus:border-primary focus:outline-none focus:ring-2 focus:ring-blue-400 ${errorInputClasses} ${disabledClasses}`}
         placeholder={placeholder}
         value={value}
         onChange={onChangeHandler}
